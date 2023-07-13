38e7c644a3a16d05d7d3a08ce02c6a557a5ad4b9
jacobmorrison <iamjacobmorrison@gmail.com>
2023-07-13 10:11:03 -0400

    Initial Commit

---
+++ Udder/UdderApp.swift
@@ -0,0 +1,17 @@
+//
+//  UdderApp.swift
+//  Udder
+//
+//  Created by Jacob Morrison on 7/13/23.
+//
+
+import SwiftUI
+
+@main
+struct UdderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
